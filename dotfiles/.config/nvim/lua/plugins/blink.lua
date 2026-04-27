288fc79391365792897e767884559f73c4a01ada
sota-6741 <207503862+sota-6741@users.noreply.github.com>
2026-04-27 22:04:08 +0900

    update: blink.cmpのビルド設定とソース設定を調整

--- dotfiles/.config/nvim/lua/plugins/blink.lua
+++ dotfiles/.config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,13 @@
 return {
   "saghen/blink.cmp",
+  -- タイムアウトを防ぐため build を一時的にコメントアウト
+  -- build = "cargo build --release",
   opts = {
     keymap = {
-      -- Enterキー(<CR>)が押されたら、常に改行を実行する
       ["<CR>"] = { "fallback" },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
     },
   },
 }
